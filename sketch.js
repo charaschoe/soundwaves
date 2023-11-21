4dee404c3176fde8cd7ef09334722c7a5f7e1a43
Glitch (capable-flaxen-growth) <none>
2023-11-21 22:33:37 +0000

    🥰🤿 Updated with Glitch

--- sketch.js
+++ sketch.js
@@ -1,9 +1,4 @@
-const words = [
-  'Marlon',
-  'Enes',
-  'Jonas',
-  'Philipp',
-]
+const words = ["Marlon", "Enes", "Jonas", "Philipp"];
 let randomIndex = 0;
 let textColor = 255;
 let bgColor = 0;
@@ -18,20 +13,23 @@ function setup() {
 function draw() {
   background(bgColor);
   fill(textColor);
-  text(words[randomIndex], width/2, height/2);
+  text(words[randomIndex], width / 2, height / 2);
 }
 
 function mouseReleased() {
   randomIndex = round(random(0, words.length - 1));
+
+  // Toggle background color
   if (bgColor === 0) {
     bgColor = 50;
   } else {
     bgColor = 0;
   }
+
+  // Toggle text color
+  textColor = 255 - textColor; // Invert the text color (assuming it starts at 255)
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
-
-
